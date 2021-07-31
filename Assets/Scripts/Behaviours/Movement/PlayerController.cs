966787eabbff082b5fbea9f5dcc34bc9822ac2d8
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2021-07-31 16:18:23 +0800

    Add: Character movement for hub stage

---
+++ Assets/Scripts/Behaviours/Movement/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerController : MonoBehaviour
+{
+    public bool isControllable = true;
+    public float speed = 5.0f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        this.rb = this.GetComponent<Rigidbody2D>();
+        this.rb.gravityScale = 0;
+    }
+
+    public void Move()
+    {
+        float directionX = 0;
+        float directionY = 0;
+
+        if(KeyMaps.IsAnyKeyPressed(KeyMaps.LEFT_MOVE_KEYS))
+        {
+            directionX = -1;
+        }
+        else if(KeyMaps.IsAnyKeyPressed(KeyMaps.RIGHT_MOVE_KEYS))
+        {
+            directionX = 1;
+        }
+
+        if(KeyMaps.IsAnyKeyPressed(KeyMaps.UP_MOVE_KEYS))
+        {
+            directionY = 1;
+        }
+        else if(KeyMaps.IsAnyKeyPressed(KeyMaps.DOWN_MOVE_KEYS))
+        {
+            directionY = -1;
+        }
+
+        Vector2 movement = new Vector2(directionX * speed, directionY * speed);
+        this.rb.velocity = movement;
+    }
+
+    void FixedUpdate()
+    {
+        if(!isControllable)
+        {
+            this.rb.velocity = new Vector2(0, 0);
+            return;
+        }
+
+        Move();
+    }
+}
--- Assets/Scripts/KeyMaps.cs
+++ Assets/Scripts/KeyMaps.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public static class KeyMaps
 {
     public static readonly KeyCode TILE_SELECTOR = KeyCode.Mouse0;
+
+    public static readonly KeyCode[] UP_MOVE_KEYS = { KeyCode.W, KeyCode.UpArrow };
+    public static readonly KeyCode[] LEFT_MOVE_KEYS = { KeyCode.A, KeyCode.LeftArrow };
+    public static readonly KeyCode[] DOWN_MOVE_KEYS = { KeyCode.S, KeyCode.DownArrow };
+    public static readonly KeyCode[] RIGHT_MOVE_KEYS = { KeyCode.D, KeyCode.RightArrow };
+
+    public static bool IsAnyKeyPressed(KeyCode[] validKeys)
+    {
+        foreach(KeyCode key in validKeys)
+        {
+            if(Input.GetKey(key))
+                return true;
+        }
+        return false;
+    }
 }
