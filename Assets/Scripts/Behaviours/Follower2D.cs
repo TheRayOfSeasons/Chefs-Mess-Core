a33424752d54e1fc8aa559168cee58636ffa0a5e
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2021-08-07 15:06:32 +0800

    Add: Follower script

---
+++ Assets/Scripts/Behaviours/Follower2D.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Follower2D : MonoBehaviour
+{
+    public GameObject target;
+
+    void Update()
+    {
+        this.transform.position = new Vector3(
+            this.target.transform.position.x,
+            this.target.transform.position.y,
+            this.transform.position.z
+        );
+    }
+}
