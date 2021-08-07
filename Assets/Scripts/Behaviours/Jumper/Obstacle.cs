752b67600a8571b9a152fbdd1b3f29af5ba51423
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2021-08-07 19:33:34 +0800

    Add: recycling logic for obstacles

--- Assets/Scripts/Behaviours/Jumper/Obstacle.cs
+++ Assets/Scripts/Behaviours/Jumper/Obstacle.cs
@@ -6,12 +6,24 @@ using UnityEngine;
 [RequireComponent(typeof(MonoTagAssigner))]
 public class Obstacle : MonoBehaviour
 {
+    private Vector3 target;
     private Vector3 direction = Vector3.left;
     private float speed = 3f;
+    private float recyclingThreshold = 0.5f;
+    private Rigidbody2D rb;
 
     void Awake()
     {
         this.GetComponent<MonoTagAssigner>().monoTag = Constants.MonoTag.JUMPER_GAME_OBSTACLE;
+    }
+
+    void Start()
+    {
+        this.target = new Vector3(
+            -9,
+            this.transform.position.y,
+            this.transform.position.z
+        );
     }
 
     void Update()
@@ -21,5 +33,11 @@ public class Obstacle : MonoBehaviour
 
         Vector3 newPosition = this.direction * this.speed * Time.deltaTime;
         this.transform.Translate(newPosition);
+
+        float distance = Vector2.Distance(this.transform.position, this.target);
+        if(distance < this.recyclingThreshold)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
