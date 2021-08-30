91204df3180fa1e9ba78c359b8e7b3f94fc36770
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2021-08-30 17:56:03 +0800

    Add: Typing Game core logic

---
+++ Assets/Scripts/Behaviours/TypingGame/Typer.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Typer : MonoBehaviour
+{
+    protected TyperWord currentWord;
+    protected int currentCharacterIndex = 0;
+    protected int currentRound = 1;
+
+    private KeyCode currentKey;
+    private bool isWon = false;
+
+    protected TyperWord GetCurrentWord()
+    {
+        Constants.Difficulty difficulty = GameManager.Instance.GetCurrentDifficulty();
+        return TyperMeta.words[difficulty];
+    }
+
+    public void Reset()
+    {
+        this.isWon = false;
+        this.currentCharacterIndex = 0;
+        this.currentRound = 1;
+        this.currentWord = this.GetCurrentWord();
+        this.currentKey = this.GetCurrentCharacterKeyCode();
+    }
+
+    public char GetCurrentCharacter()
+    {
+        return this.currentWord.word[this.currentCharacterIndex];
+    }
+
+    public KeyCode GetCurrentCharacterKeyCode()
+    {
+        char character = this.GetCurrentCharacter();
+        KeyCode key = (KeyCode) System.Enum.Parse(typeof(KeyCode), character.ToString().ToUpper());
+        return key;
+    }
+
+    void Start()
+    {
+        this.Reset();
+    }
+
+    void HandleWin()
+    {
+        this.isWon = true;
+        Debug.Log("Typer has been won");
+    }
+
+    void OnCharacterPress()
+    {
+        char previousCharacter = this.GetCurrentCharacter();
+
+        int lastIndex = this.currentWord.word.Length - 1;
+        int oldIndex = this.currentCharacterIndex;
+        int newIndex = this.currentCharacterIndex < lastIndex
+            ? this.currentCharacterIndex + 1
+            : 0;
+        this.currentCharacterIndex = newIndex;
+        this.currentKey = this.GetCurrentCharacterKeyCode();
+
+        this.AfterCharacterPress(previousCharacter);
+        if(newIndex == lastIndex)
+        {
+            this.currentRound++;
+            if(this.currentRound == this.currentWord.rounds)
+            {
+                this.HandleWin();
+            }
+        }
+    }
+
+    void AfterCharacterPress(char character)
+    {
+        // add here your custom logic for applying extra effects
+        // or animation after pressing the right key
+    }
+
+    void Update()
+    {
+        if(this.isWon)
+            return;
+
+        if(Input.GetKeyDown(this.currentKey))
+        {
+            this.OnCharacterPress();
+        }
+    }
+}
---
+++ Assets/Scripts/Behaviours/TypingGame/TyperMeta.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TyperWord
+{
+    public string word;
+    public int rounds;
+
+    public TyperWord(string word, int rounds)
+    {
+        this.word = word;
+        this.rounds = rounds;
+    }
+}
+
+public class TyperMeta
+{
+    public static Dictionary<Constants.Difficulty, TyperWord> words = new Dictionary<Constants.Difficulty, TyperWord>() {
+        {Constants.Difficulty.EASY, new TyperWord("brocolis", 3)},
+        {Constants.Difficulty.MEDIUM, new TyperWord("tomatoes", 3)},
+        {Constants.Difficulty.HARD, new TyperWord("carrots", 4)}
+    };
+}
