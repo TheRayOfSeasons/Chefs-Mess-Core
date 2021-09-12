0695adfc5a62de0a75a23347d4da174b9ed2faed
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2021-09-12 19:20:15 +0800

    Add: QuestLockedModal

---
+++ Assets/Scripts/UI/Modals/QuestLockedModal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UIComponents;
+
+public class QuestLockedModal : Modal
+{
+    public Text titleComponent;
+
+    public void SetTitle(string title)
+    {
+        this.titleComponent.text = title;
+    }
+}
