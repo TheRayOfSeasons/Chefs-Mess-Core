ffa5682b993f610321ec94813f6528ff94a2864e
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2021-10-10 19:39:45 +0800

    Add: cutscene controller

---
+++ Assets/Scripts/UI/CutsceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CutsceneController : MonoBehaviour
+{
+    public Animator animator;
+
+    void Awake()
+    {
+        this.animator = this.GetComponent<Animator>();
+    }
+
+    void OnEnable()
+    {
+        this.animator.Play("Intro");
+    }
+
+    public void EndAnimation()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
